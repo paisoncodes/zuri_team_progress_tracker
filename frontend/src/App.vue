aebe99a423b835b7eebad886ab85a3d53790b893
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2021-10-13 10:25:18 -0700

    Initial setup of progress tracker app with Vue and TailwindCss

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <!-- <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> -->
+  </div>
+  <router-view/>
+</template>
+
+<style>
+
+</style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-red-100 text-center">
+    This is the official page of the project tracker app
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+}
+</script>
