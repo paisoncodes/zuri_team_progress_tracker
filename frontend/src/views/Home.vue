86b50531ecba5bf56aa5372edef7aa0cc725b6ac
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2021-10-13 21:55:06 -0700

    Addition of Footer to Home page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="bg-red-100 text-center">
+  <div class="text-center">
     This is the official page of the project tracker app
     <HomeContainer />
+    <Footer />
   </div>
 </template>
 
 <script>
 import HomeContainer from '../components/HomeContainer.vue'
+import Footer from '../components/Footer.vue'
 
 export default {
-  components: { HomeContainer },
+  components: { HomeContainer, Footer },
   name: 'Home',
 }
 </script>
