5c9d732e945ee49d8cc5116af9a8b1b0fdf43bb3
Boye <rippyblogger@gmail.com>
2021-10-14 20:49:38 +0400

    2021 page set up

--- frontend/src/views/TwentyOne.vue
+++ frontend/src/views/TwentyOne.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="bg-white text-center flex flex-col">
     <NavbarComponent/>
+    <Subscribe/>
+    <TwentyOneContainer/>
 
   </div>
 </template>
 
 <script>
 import NavbarComponent from '../components/NavbarComponent.vue'
+import Subscribe from '../components/Subscribe.vue'
+import TwentyOneContainer from '../components/TwentyOneContainer.vue'
 
 export default {
-  components: { NavbarComponent},
+  components: { NavbarComponent, Subscribe, TwentyOneContainer},
   name: 'TwentyOne',
 }
 </script>
