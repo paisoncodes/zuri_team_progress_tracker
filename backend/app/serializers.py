f2af08c4e9902a5b6ae31d94097ae6cb0628635f
noornee <kendrickbon11@gmail.com>
2021-10-14 08:56:29 +0100

    user model create and update view

--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -6,14 +6,20 @@ from .models import Intern, User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["first_name", "last_name", "email", "address", "city", "state"]
+        fields = ["id", "first_name", "last_name", "email", "address", "city", "state", "active","staff","admin","date" ]
+
+class UpdateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "first_name", "last_name", "email", "address", "city", "state"]
+        extra_kwargs = { "email": {"required": False} }
 
 class InternSerializer(serializers.Serializer):
     
     name = serializers.CharField(max_length=100)
     stack = serializers.CharField(max_length = 100)
     job = serializers.CharField(max_length=100)
-    batch = serializers.IntegerField(max_length=100)
+    batch = serializers.IntegerField()
 
     def create(self, validated_data):
         return Intern.objects.create(**validated_data)
