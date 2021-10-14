c57e5f25b9a7f60605b1e5ccc32abc66bc7e765d
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2021-10-14 10:51:37 -0700

    Update to footer responsiveness

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,29 +1,29 @@
 <template>
     <div class="max-w-6xl mx-auto p-5 text-brand-gray-dark-1">
-        <div class="sm:flex sm:justify-between sm:space-x-3 text-sm">
-            <div class="mb-4 sm:mb-0 text-lg text-center sm:text-left font-medium">
+        <div class="md:flex md:justify-between md:space-x-3 text-sm">
+            <div class="mb-4 md:mb-0 text-lg text-center md:text-left font-medium">
                 ZURI.TEAM
             </div>
-            <div class="mb-4 sm:mb-0 text-center sm:text-left">
+            <div class="mb-4 md:mb-0 text-center md:text-left">
                 <h2 class="mb-2 text-lg font-medium">Quick Links</h2>
                 <p class="mb-2">Home</p>
                 <p class="mb-2">Curriculum</p>
                 <p class="mb-2">FAQ</p>
                 <p class="mb-2">Terms and Conditions</p>
             </div>
-            <div class="mb-4 sm:mb-0 text-center sm:text-left">
+            <div class="mb-4 md:mb-0 text-center md:text-left">
                 <h2 class="mb-2 text-lg font-medium">Connect with us</h2>
                 <p class="mb-2">Email</p>
                 <p class="mb-2">Blog</p>
                 <p class="mb-2">Store</p>
             </div>
-            <div class="mb-4 sm:mb-0 text-center sm:text-left">
+            <div class="mb-4 md:mb-0 text-center md:text-left">
                 <h2 class="mb-2 text-lg font-medium">Office Address</h2>
                 <p>8 Jubllie-CMD Road,</p>
                 <p>Magodo, Lagos State</p>
             </div>
             <div>
-                <h2 class="mb-2 text-lg font-medium text-center sm:text-right">Follow us</h2>
+                <h2 class="mb-2 text-lg font-medium text-center md:text-right">Follow us</h2>
                 <div class="flex flex-1 justify-center lg:justify-between space-x-6">
                     <div><a href=""><img src="../assets/instagram.png" alt="Instagram Logo"></a></div>
                     <div><a href=""><img src="../assets/facebook.png" alt="Facebook Logo"></a></div>
