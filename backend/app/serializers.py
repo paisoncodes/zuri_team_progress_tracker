7ff5dfcf072905454d643f8eab3bdc3cb451346e
Olatunji <iamkomolafe.o.s@gmail.com>
2021-10-14 13:22:11 +0100

    updated user serializer

--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -3,16 +3,30 @@ from rest_framework import serializers
 from .models import Intern, User
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id", "first_name", "last_name", "email", "address", "city", "state"]
+class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    first_name = serializers.CharField(max_length=100)
+    last_name = serializers.CharField(max_length=100)
+    email = serializers.EmailField(max_length=200)
+    # password = serializers.PasswordField(max_length=100)
+    address = serializers.CharField(max_length=200)
+    city = serializers.CharField(max_length=100)
+    state = serializers.CharField(max_length=100)
 
-class UpdateUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id", "first_name", "last_name", "email", "address", "city", "state"]
-        extra_kwargs = { "email": {"required": False} }
+    def create(self, validated_data):
+        return User.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.address = validated_data.get('address', instance.address)
+        instance.city = validated_data.get('city', instance.city)
+        instance.state = validated_data.get('state', instance.state)
+
+        instance.save()
+        return instance
+    
 
 class InternSerializer(serializers.Serializer):
     
