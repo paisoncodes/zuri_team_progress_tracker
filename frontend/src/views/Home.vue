b201288be281edcbc412d92600ff7e26dcb5e290
Emmanuella Oghenekaro <oghenekaro57@gmail.com>
2021-10-14 08:16:10 +0100

    made some changes

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,16 +2,18 @@
   <div class="text-center">
     This is the official page of the project tracker app
     <HomeContainer />
+    <HomeContainer />
     <Footer />
   </div>
 </template>
 
 <script>
 import HomeContainer from '../components/HomeContainer.vue'
+import NavbarComponent from '../components/NavbarComponent.vue'
 import Footer from '../components/Footer.vue'
 
 export default {
-  components: { HomeContainer, Footer },
+  components: { HomeContainer,NavbarComponent, Footer },
   name: 'Home',
 }
 </script>
