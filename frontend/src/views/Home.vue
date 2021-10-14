259bdfdf9df397e674e33db8147e745c25877575
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2021-10-13 21:04:01 -0700

    Creation of Footer

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,14 +2,16 @@
   <div class="text-center">
     <div class="bg-brand-gray-light-2">This is the official page of the project tracker app</div> 
     <HomeContainer />
+    <Footer />
   </div>
 </template>
 
 <script>
 import HomeContainer from '../components/HomeContainer.vue'
+import Footer from '../components/Footer.vue'
 
 export default {
-  components: { HomeContainer },
+  components: { HomeContainer, Footer },
   name: 'Home',
 }
 </script>
