f16d885f15ea71dadd93290b1541b67d900c446c
Phosah <55056242+Phosah@users.noreply.github.com>
2021-10-13 21:19:02 -0700

    Revert "Worked on Current Job Placement section in home screen"

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,4 @@
 <template>
-  <div class="bg-red-100 ">
-    <p class="text-center">This is the official page of the project tracker app</p>
-    <HomeContainer />
   <div>
     <!--This is the official page of the project tracker app
     <HomeContainer />-->
