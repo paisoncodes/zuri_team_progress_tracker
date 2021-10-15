42af3723a2d8fa4c1ccb520bb98850c15e286c82
Komolafe Olatunji <68442675+paisoncodes@users.noreply.github.com>
2021-10-15 17:10:38 +0100

    Updated intern serializer

--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -55,25 +55,9 @@ class UserUpdateSerializer(serializers.Serializer):
 
 class InternSerializer(serializers.Serializer):
     
-    name = serializers.CharField(max_length=100,required=False)
-    stack = serializers.CharField(max_length = 100,required=False)
-    job = serializers.CharField(max_length=100,required=False)
-    batch = serializers.IntegerField(required=False)
-
-    
-
-    def create(self, validated_data):
-        return Intern.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-
-        instance.name = validated_data.get('name', instance.name)
-        instance.stack = validated_data.get('stack', instance.stack)
-        instance.job = validated_data.get('job', instance.job)
-        instance.batch = validated_data.get('batch', instance.batch)
-
-        instance.save()
-        return instance
+    class Meta:
+        model: Intern
+        fields = ['username', 'full_name', 'stack', 'state', 'about', 'batch', 'is_employed']
 
 class JobSerializer(serializers.ModelSerializer):
     class Meta:
