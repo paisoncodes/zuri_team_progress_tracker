62832cced977d8176cacb8ef883b75b077a4e632
temitayopelumi <olalekantemitayo7@gmail.com>
2021-10-15 12:20:47 +0100

    deploy

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -5,7 +5,9 @@ DEBUG = False
 SECRET_KEY = os.environ.get("SECRET_KEY")
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'zuri-progress-tracker.herokuapp.com']
 
+bashCommand = "heroku config:get DATABASE_URL -a zuri-progress-tracker" #Use your app_name
 
+output = subprocess.check_output(["bash",'-c', bashCommand]).decode("utf-8")
 DATABASES['default'] = dj_database_url.config(default=output,conn_max_age=600, ssl_require=True)
 # DATABASES = {
 #     "default": {
