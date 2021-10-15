93f04a8efab9c671c708742fab7f8710c7210f35
Boye <rippyblogger@gmail.com>
2021-10-15 11:38:28 +0400

    Test homepage view

--- frontend/src/components/TwentyOneContainer.vue
+++ frontend/src/components/TwentyOneContainer.vue
@@ -1,31 +1,35 @@
 <template>
 
-<div class="py-8 mx-auto">
+<div class="py-8 ">
 
-    <nav class="flex flex-col text-sm sm:flex-row">
-        <button class="block py-4 text-brand-gray-light padothers hover:text-brand-gray-dark-1 focus:outline-none">
+    <nav class="flex flex-col text-sm sm:flex-row px-6 mx-auto max-w-7xl">
+        <button class="block py-4 text-brand-gray-light-1  w-1/6 hover:text-brand-gray-dark-1 focus:outline-none">
             OVERVIEW 
         </button>
         
-        <button class="block py-4 font-medium bg-brand-red-light-1 padtab1 hover:text-blue-500 focus:outline-none text-brand-gray-dark-1 border-brand-gray-light">
+        <button class="block py-4 font-medium bg-brand-red-light padtab1 hover:text-blue-500 focus:outline-none text-brand-gray-dark-1 border-brand-gray-light bg-brand-red-light-1 w-1/6">
             2021 <br> (10,000 interns)
         </button>
         
-        <button class="block py-4 text-brand-gray-light padothers hover:text-brand-gray-dark-1 focus:outline-none">
+        <button class="block py-4 text-brand-gray-light padothers w-1/6 hover:text-brand-gray-dark-1 focus:outline-none">
             2020 <br> (10,000 interns)
         </button>
         
-        <button class="block py-4 text-brand-gray-light padothers hover:text-brand-gray-dark-1 focus:outline-none">
+        <button class="block py-4 text-brand-gray-light w-1/6 padothers hover:text-brand-gray-dark-1 focus:outline-none">
             2019 <br> (10,000 interns)
         </button>
-        <button class="block py-4 text-brand-gray-light padothers hover:text-brand-gray-dark-1 focus:outline-none">
+        <button class="block py-4 text-brand-gray-light w-1/6 padothers hover:text-brand-gray-dark-1 focus:outline-none">
             2018 <br> (10,000 interns)
         </button>
     </nav>
 
-    <div class="bg-brand-red-light-1 ">
-
-
+    <div class="px-6 py-10 mx-auto max-w-7xl bg-brand-red-light-1 ">
+        <MeetOurIntern />
+        <InternProfile />
+        <Interns />
+        <FilterButton />
+        <experinceTracker />
+        <Footer />
     </div>
 
     
@@ -33,7 +37,22 @@
 </template>
 
 <script>
+import MeetOurIntern from "../components/MeetOurIntern.vue";
+import InternProfile from "../components/InternProfile.vue";
+import Interns from "../components/Interns.vue";
+import FilterButton from "../components/FilterButton.vue";
+import experinceTracker from "../components/experinceTracker.vue";
+import Footer from "../components/Footer.vue";
+
 export default {
-    name: "TwentyOneContainer.vue"
+    name: "TwentyOneContainer.vue",
+    components:{
+        MeetOurIntern,
+    InternProfile,
+    Interns,
+    FilterButton,
+    experinceTracker,
+    Footer,
+    }
 }
 </script>
