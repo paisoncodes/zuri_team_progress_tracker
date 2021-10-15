e82618e997336e1bf8c3fb3102fce2238e7e4b83
temitayopelumi <olalekantemitayo7@gmail.com>
2021-10-15 12:05:27 +0100

    deploy

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -6,7 +6,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'zuri-progress-tracker.herokuapp.com']
 
 
-DATABASES[‘default’] = dj_database_url.config(default=output,conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(default=output,conn_max_age=600, ssl_require=True)
 # DATABASES = {
 #     "default": {
 #         "ENGINE": "django.db.backends.postgresql_psycopg2",
