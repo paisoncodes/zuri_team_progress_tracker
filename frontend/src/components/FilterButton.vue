22181a2036523a5b1028222388ae06516bbad4a6
Oluwaseyi <seyiewebajo@gmail.com>
2021-10-16 07:18:31 +0100

    fix filter button responsiveness

--- frontend/src/components/FilterButton.vue
+++ frontend/src/components/FilterButton.vue
@@ -1,23 +1,27 @@
 <template>
-    <div class="mx-auto flex justify-around items-center bg-brand-red-light-1 p-5">
-        <button class="bg-brand-red-dark hover:bg-white text-white hover:text-brand-red-dark font-normal px-6 py-2 rounded-full text-sm">
+    <div class="bg-brand-red-light-1">
+      <div class="py-6">
+        <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-5">
+          <button class="bg-brand-red-dark hover:bg-white text-white hover:text-brand-red-dark font-normal sm:px-6 lg:px-2  py-2 rounded-full text-sm" @Click="AllStack">
             All Stack (27,095)
-        </button>
-        <button class="bg-brand-red-light-1 border-solid border-2 border font-normal px-6 py-2 rounded-full text-sm">
+          </button>
+          <button class="bg-brand-red-light-1 border-solid border-2 border font-normal sm:px-6 lg:px-2  py-2 rounded-full text-sm" @Click="HTML">
             HTML (3,853)
-        </button>
-        <button class="bg-brand-red-light-1 border-solid border-2 border font-normal px-6 py-2 rounded-full text-sm">
+          </button>
+          <button class="bg-brand-red-light-1 border-solid border-2 border font-normal sm:px-6 lg:px-2  py-2 rounded-full text-sm" @Click="CSS">
             CSS (3,853)
-        </button>
-        <button class="bg-brand-red-light-1 border-solid border-2 border font-normal px-6 py-2 rounded-full text-sm">
+          </button>
+          <button class="bg-brand-red-light-1 border-solid border-2 border font-normal sm:px-6 lg:px-2  py-2 rounded-full text-sm" @Click="Javascript">
             Javascript (6,437)
-        </button>
-        <button class="bg-brand-red-light-1 border-solid border-2 border font-normal px-6 py-2 rounded-full text-sm">
+          </button>
+          <button class="bg-brand-red-light-1 border-solid border-2 border font-normal sm:px-6 lg:px-2 py-2 rounded-full text-sm" @Click="UiUx">
             UI/UX Design (7,787)
-        </button>
-        <button class="bg-brand-red-light-1 border-solid border-2 border font-normal px-6 py-2 rounded-full text-sm">
+          </button>
+          <button class="bg-brand-red-light-1 border-solid border-2 border font-normal sm:px-6 lg:px-2  py-2 rounded-full text-sm" @Click="Java">
             Java (924)
-        </button>
+          </button>
+        </div>
+      </div>
     </div>
 </template>
 
@@ -25,7 +29,30 @@
 export default {
     name: 'FilterButton',
     data(){
-        
+
+    },
+    methods: {
+        AllStack() {
+           
+        },
+        HTML() {
+
+        },
+        CSS() {
+
+        },
+        Javascript() {
+
+        },
+        UiUx() {
+
+        },
+        Java() {
+
+        }
+    },
+    async created () {
+
     }
 }
 </script>
