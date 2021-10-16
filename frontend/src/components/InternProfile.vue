e61d9735502c1e88be8749ac7f9c12185f054c93
unknown <izik4004@gmail.com>
2021-10-16 16:37:23 +0100

    called api

--- frontend/src/components/InternProfile.vue
+++ frontend/src/components/InternProfile.vue
@@ -1,15 +1,15 @@
 <template>
 <div class="mx-auto max-w-7xl ">
     <!-- component -->
-<div v-for="(profile, i) in profiles" :key="i" class="py-6 mx-auto">
+<div v-for="profile in profiles" v-bind:key="profile.id" class="py-6 mx-auto">
   <div class="mx-auto shadow-xs md:overflow-hidden sm:flex bg-brand-red-light-2 md:h-full">
   
     <img class="w-full md:h-full lg:w-60 " v-bind:src="profile.picture" v-bind:alt="profile.alt" />
  
     <div class="px-6 text-left">
-      <h1 class="text-4xl font-bold leading-loose text-brand-gray-dark-1">{{profile.name}}</h1>
-      <h3 class="py-2 text-xl leading-normal text-brand-gray-dark-1 ">{{profile.role}}</h3>
-      <h4 class="py-2 text-base leading-tight text-brand-gray-light">{{profile.stage}}</h4>
+      <h1 class="text-4xl font-bold leading-loose text-brand-gray-dark-1">{{profile.full_name}}</h1>
+      <h3 class="py-2 text-xl leading-normal text-brand-gray-dark-1 ">{{profile.stack}}</h3>
+      <h4 class="py-2 text-base leading-tight text-brand-gray-light">{{profile.username}}</h4>
       <p class="text-base text-brand-gray-light">{{profile.about}}</p>
     </div>
   </div>
@@ -18,25 +18,34 @@
 </template>
 
 <script>
+import axios from "axios"
+
+
+
+
 export default {
   name: 'profiles',
-  data(){
+    data(){
     return {
-      profiles:[
-        {
-          name:"John Doe",
-          role: 'Senior Digital Product Designer @andela',
-          stage:  'Zuri Training Finalist',
-           picture: require('../assets/chuks.png'),
-           alt: 'Mariam Danjuma',
-           about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Accumsan sit amet in morbi dolor nunc, at. At scelerisque sed leo in pharetra. Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
-        },
-        
-      ]
+      profiles:[]
     }
   },
+  created () {
+    axios
+     .get ('https://zuri-progress-tracker.herokuapp.com/api/v1/interns/')
+      .then(res => {
+        this.profiles = res.data.slice(0,1);
+      })
+         
+        .catch(error => {
+          console.log(error.response)
+        })
+      }
+      
+  }
+
   
-}
+
 </script>
 
 <style>
--- frontend/src/components/Interns.vue
+++ frontend/src/components/Interns.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="mx-auto md:max-w-full lg:py-10 ">
     <div class="grid lg:grid-cols-4 md:grid-cols-2 md:gap-6">
-    <div v-for="(intern, i) in interns" :key="i" class="w-full mx-auto overflow-hidden text-left transition-shadow duration-300 rounded-sm">
+    <div v-for="intern in interns" v-bind:key="intern.id"  class="w-full mx-auto overflow-hidden text-left transition-shadow duration-300 rounded-sm">
         <a href="/" aria-label=""><img class="object-cover w-full h-60" v-bind:src="intern.picture" v-bind:alt="intern.alt" /></a>
         <div  class="py-4">
          
           <a href="/" aria-label="" class="inline-block text-black transition-colors duration-200 hover:text-deep-purple-accent-700">
             <p class="text-2xl font-bold leading-5 text-brand-gray-dark-1">
-             {{ intern.name}}
+             {{ intern.full_name}}
             </p>
           </a>
           <p class="py-2 text-brand-gray-dark-1">
-          {{intern.track}}
+          {{intern.stack}}
           </p>
           <div class="flex space-x-4">
             <span class="text-brand-gray-light">
-         {{intern.stage}}
+         Zuri Training Finalist
           </span>
           </div>
         </div>
@@ -26,42 +26,27 @@
 
 
 <script>
+
+import axios from "axios"
+
 export default {
   name: 'Interns',
   data(){
     return {
-      interns:[
-        {
-          name:"Mariam Danjuma",
-          track: 'UI/UX Designer/Figma',
-          stage:  'Zuri Training Finalist',
-           picture: require('../assets/mariam.png'),
-           alt: 'Mariam Danjuma'
-        },
-        {
-          name:"Felix Abasi Franklin",
-          track: 'UI/UX Designer/Figma',
-          stage:  'Zuri Training Finalist',
-          picture: require('../assets/felix.png'),
-          alt: 'Felix Abasi Franklin'
-        },
-        {
-         name:"Afolanyan Gbeminiyi",
-          track: 'UI/UX Designer/Figma',
-          stage:  'Zuri Training Finalist',
-          picture: require('../assets/afolayan.png'),
-           alt: 'Afolanyan Gbeminiyi'
-        },
-        {
-         name:"Namnso Ukpanah",
-          track: 'UI/UX Designer/Figma',
-          stage:  'Zuri Training Finalist',
-          picture: require('../assets/namso.png'),
-           alt: 'Namnso Ukpanah'
-        },
-      ]
-    }
+      interns:[],
+  }
   },
+   created () {
+    axios
+     .get ('https://zuri-progress-tracker.herokuapp.com/api/v1/interns/')
+      .then(res => {
+        this.interns = res.data.slice(0,12);
+      })
+         
+        .catch(error => {
+          console.log(error.response)
+        })
+      }
   
 }
 </script>
