fd92f5999f50586c098e7989bb1415b4183fdc80
tjfaithpro <tjbenbiz@gmail.com>
2021-10-15 22:25:51 -0700

    adjusted login modal

--- frontend/src/components/LoginModal.vue
+++ frontend/src/components/LoginModal.vue
@@ -7,7 +7,18 @@
       aria-modal="true"
     >
       <div
-        class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+        class="
+          flex
+          items-end
+          justify-center
+          min-h-screen
+          pt-4
+          px-4
+          pb-20
+          text-center
+          sm:block
+          sm:p-0
+        "
       >
         <div
           class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
@@ -22,17 +33,30 @@
         >
 
         <div
-          class="inline-block align-bottom bg-white text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+          class="
+            inline-block
+            align-bottom
+            bg-white
+            text-left
+            overflow-hidden
+            shadow-xl
+            transform
+            transition-all
+            sm:my-8
+            sm:align-middle
+            sm:max-w-lg
+            sm:w-full
+          "
         >
-          <div @click="showLogin" class=" float-right pt-8 pr-8 cursor-pointer">
+          <div @click="showLogin" class="float-right pt-8 pr-8 cursor-pointer">
             <i
-              class="fa fa-times text-gray-dark-1  hover:text-brand-gray-light"
+              class="fa fa-times text-gray-dark-1 hover:text-brand-gray-light"
               aria-hidden="true"
             ></i>
           </div>
-          <form class="m-8  ">
+          <form class="m-8">
             <!-- CONTENT -->
-            <div class=" sm:text-left">
+            <div class="sm:text-left">
               <h1 class="mb-6 text-2xl text-gray-dark-1">LOGIN</h1>
               <p class="mb-3 text-sm text-gray-dark-1">PASSWORD</p>
               <input
@@ -40,22 +64,43 @@
                 name=""
                 id=""
                 placeholder="Enter your password here"
-                class="w-full px-2 py-2 text-left outline-black "
+                class="w-full px-2 py-2 text-left outline-black"
               />
             </div>
 
             <!-- FOOTER -->
             <div class="mb-8 mt-5 sm:flex sm:flex-row-reverse">
+              
               <button
-                @click="showLogin"
-                class="px-6 py-2 ml-4 text-white border-2 text-brand-gray-dark-1 border-brand-gray-dark-2 hover:bg-brand-gray-light hover:text-white"
-              >
-                Cancel
-              </button>
-              <button
-                class="px-6 py-2 ml-4 text-white bg-brand-gray-dark-1 hover:bg-brand-gray-light hover:text-white"
+                class="
+                  px-8
+                  py-2
+                  ml-4
+                  text-white
+                  border
+                  border-brand-gray-dark-1
+                  bg-brand-gray-dark-1
+                  hover:bg-brand-gray-light
+                  hover:text-white
+                "
               >
                 Login
+              </button>
+              <button
+                @click="showLogin"
+                class="
+                  px-8
+                  py-2
+                  ml-4
+                  text-white
+                  border
+                  text-brand-gray-dark-1
+                  border-brand-gray-dark-2
+                  hover:bg-brand-gray-light
+                  hover:text-white
+                "
+              >
+                Cancel
               </button>
             </div>
           </form>
