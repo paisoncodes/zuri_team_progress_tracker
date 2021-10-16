34355f7406eb32938ee93211dd55996b494d201d
Oluwaseyi <seyiewebajo@gmail.com>
2021-10-16 18:00:04 +0100

    filter button functionality

--- frontend/src/components/ExperienceTracker.vue
+++ frontend/src/components/ExperienceTracker.vue
@@ -138,7 +138,7 @@ export default {
   },
  computed: {
         ...mapGetters({
-             getAllInterns: 'allInterns',
+             getAllInterns: 'getAllStack',
              userJob:'allUserjobs'
         })
     },
