30b7d0f421b86c35f01f94a5f5e701b396fabaeb
Petec0x0 <udehonyedikachi01@gmail.com>
2021-10-16 19:57:12 +0100

    Implement progress bar functionality

--- frontend/src/components/CurrentJobPlacement.vue
+++ frontend/src/components/CurrentJobPlacement.vue
@@ -7,21 +7,50 @@
         </div>
         <div class="relative pt-5">
             <div class="overflow-hidden h-5 text-lg flex rounded-xl bg-gray-200">
-                <div style="width:60%"
-                    class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-brand-gray-dark-1">
+                <div :style="'width:' + progress_percent +'%'"
+                    class="test-try shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-brand-gray-dark-1">
                 </div>
             </div>
         </div>
-        <p class="emp-status mt-5 text-left text-xs">1,54500,000 / 2,056,789 Employed</p>
+        <p class="emp-status mt-5 text-left text-xs">{{total_employed}} / {{total_finalist}} Employed</p>
         <div class="mt-10 mb-8 text-brand-gray-blue text-left">
-            <h2 class="text-xs"><a href="#">VIEW COMPLETE PROGRESS ></a></h2>
+            <h2 class="text-xs"><a href="#">VIEW COMPLETE PROGRESS{{progress_percent}} {{'\xa0'}}≻</a></h2>
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'CurrentJobPlacement'
+        name: 'CurrentJobPlacement',
+        props:['total_finalist', 'total_employed'],
+        data(){
+            return {
+                progress_percent: this.get_progress_percent(),
+                new_total_employed: 0,
+                new_total_finalist: 0
+            }
+        },
+        methods:{
+            get_progress_percent(){
+                this.new_total_employed = this.total_employed
+                this.new_total_finalist = this.total_finalist
+                console.log(this.new_total_employed, this.new_total_finalist)
+                return (this.new_total_employed/this.new_total_finalist)*100
+            }
+        },
+        watch: {
+            total_finalist (val, oldVal) {
+              if (val !== oldVal){
+                this.new_total_finalist = val;
+              }
+            },
+            total_employed (val, oldVal) {
+              if (val !== oldVal){
+                this.new_total_employed = val;
+                this.progress_percent = this.get_progress_percent()
+              }
+            }
+        }
     }
 </script>
 
