cda93db2a4f0778e5f5cea5fac5a47f584dc7e1f
Phosah <55056242+Phosah@users.noreply.github.com>
2021-10-15 19:35:11 -0700

    Revert "Modified header "

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <div class="bg-white text-center flex flex-col">
-      <HeaderNavbar/>
+      <NavbarComponent/>
+      <Subscribe/>
       <TestContainer class="padTop"/>
     </div>
     <Footer />
@@ -9,14 +10,13 @@
 </template>
 
 <script>
-// import NavbarComponent from '../components/NavbarComponent.vue'
-import HeaderNavbar from '../components/HeaderNavComponent.vue'
-// import Subscribe from "../components/Subscribe.vue"
+import NavbarComponent from '../components/NavbarComponent.vue'
+import Subscribe from "../components/Subscribe.vue"
 import TestContainer from '../components/TabContainer.vue'
 import Footer from '../components/Footer.vue'
 
 export default {
-  components: { HeaderNavbar, TestContainer, Footer },
+  components: { NavbarComponent, Subscribe, TestContainer, Footer },
   name: 'Home',
 }
 </script>
