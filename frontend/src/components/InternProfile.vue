9453a38be251243f0ebdd411702aaf75e03e4a05
unknown <izik4004@gmail.com>
2021-10-16 16:55:56 +0100

    fixes

--- frontend/src/components/InternProfile.vue
+++ frontend/src/components/InternProfile.vue
@@ -4,9 +4,9 @@
 <div v-for="profile in profiles" v-bind:key="profile.id" class="py-6 mx-auto">
   <div class="mx-auto shadow-xs md:overflow-hidden sm:flex bg-brand-red-light-2 md:h-full">
   
-    <img class="w-full md:h-full lg:w-60 " v-bind:src="profile.picture" v-bind:alt="profile.alt" />
+    <img class="object-cover w-full h-60 lg:w-60 " v-bind:src="profile.picture" v-bind:alt="profile.alt" />
  
-    <div class="px-6 text-left">
+    <div class="px-6 py-4 text-left">
       <h1 class="text-4xl font-bold leading-loose text-brand-gray-dark-1">{{profile.full_name}}</h1>
       <h3 class="py-2 text-xl leading-normal text-brand-gray-dark-1 ">{{profile.stack}}</h3>
       <h4 class="py-2 text-base leading-tight text-brand-gray-light">{{profile.username}}</h4>
--- frontend/src/components/Interns.vue
+++ frontend/src/components/Interns.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mx-auto md:max-w-full lg:py-10 ">
     <div class="grid lg:grid-cols-4 md:grid-cols-2 md:gap-6">
-    <div v-for="intern in interns" v-bind:key="intern.id"  class="w-full mx-auto overflow-hidden text-left transition-shadow duration-300 rounded-sm">
+    <div v-for="intern in interns" v-bind:key="intern.id"  class="w-full py-4 mx-auto overflow-hidden text-left transition-shadow duration-300 rounded-sm">
         <a href="/" aria-label=""><img class="object-cover w-full h-60" v-bind:src="intern.picture" v-bind:alt="intern.alt" /></a>
         <div  class="py-4">
          
@@ -40,7 +40,7 @@ export default {
     axios
      .get ('https://zuri-progress-tracker.herokuapp.com/api/v1/interns/')
       .then(res => {
-        this.interns = res.data.slice(0,12);
+        this.interns = res.data.slice(0,4);
       })
          
         .catch(error => {
