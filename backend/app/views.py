c46646837cc4a0146f27666ea0af0c33da394cf5
Komolafe Olatunji <68442675+paisoncodes@users.noreply.github.com>
2021-10-25 09:20:54 +0100

    Update views.py

--- backend/app/views.py
+++ backend/app/views.py
@@ -564,7 +564,7 @@ class GetStacksPerBatch(APIView):
                 )
             for stack in year:
                 stacks.append(stack.name)
-            data = {"stacks": stack, "stack_data": stack_detail}
+            data = {"stacks": stacks, "stack_data": stack_detail}
             return Response(data, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"Message": f"{e}"}, status.HTTP_400_BAD_REQUEST)
