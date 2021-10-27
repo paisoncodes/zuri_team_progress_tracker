3389841f1e53bef2cb54cf2d2d9513b6418fa03d
paisoncodes <iamkomolafe.o.s@gmail.com>
2021-10-27 17:00:44 +0100

    fixed id for users

--- backend/custom_admin/serializers.py
+++ backend/custom_admin/serializers.py
@@ -5,45 +5,20 @@ from app.models import *
 from app.serializers import *
 
 class AdminUserSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    full_name = serializers.CharField(max_length=200)
-    email = serializers.EmailField(max_length=200)
-    permissions = serializers.CharField(max_length=1, help_text="\'S' for staff, \'A' for admin", required=False)
-    staff = serializers.BooleanField(default=False)
-    admin =serializers.BooleanField(default=False)
+    id                  = serializers.ReadOnlyField()
+    full_name           = serializers.CharField(max_length=200)
+    email               = serializers.EmailField(max_length=200)
+    permissions         = serializers.CharField(max_length=1, help_text="\'S' for staff, \'A' for admin", required=False)
+    staff               = serializers.BooleanField(default=False)
+    admin               = serializers.BooleanField(default=False)
 
     def create(self, validated_data):
         return User.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.full_name = validated_data.get("first_name", instance.full_name)
-        instance.email = validated_data.get("email", instance.email)
-        instance.permissions = validated_data.get("permission", instance.permissions)
-        instance.staff = validated_data.get("staff", instance.staff)
-        instance.admin = validated_data.get("admin", instance.admin)
+        instance.full_name      = validated_data.get("first_name", instance.full_name)
+        instance.email          = validated_data.get("email", instance.email)
+        instance.permissions    = validated_data.get("permission", instance.permissions)
+        instance.staff          = validated_data.get("staff", instance.staff)
+        instance.admin          = validated_data.get("admin", instance.admin)
         return instance
-
-class InternAdminSerializer(serializers.ModelSerializer):
-    stack = StackSerializer(many=True)
-    class Meta:
-        model = Intern
-        fields = [
-            "id",
-            "username",
-            "full_name",
-            "stack",
-            "state",
-            "gender",
-            "about",
-            "batch",
-            "current_salary",
-            "employed",
-            "picture",
-        ]
-
-    def create(self, validated_data):
-        stacks_data = validated_data.pop('stack')
-        intern = Intern.objects.create(**validated_data)
-        for stack_data in stacks_data:
-            Stack.objects.create(intern=intern, **stack_data)
-        return intern
--- backend/custom_admin/urls.py
+++ backend/custom_admin/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path
-from django.contrib import admin
 
 from custom_admin.views import (
     UserAdminCreateView,
-    UserAdminUpdateView,
-    StackAdminCreateView,
-    StackAdminUpdateView,
-    InternsAdminView,
-    InternAdminUpdateView,
+    UserAdminUpdateView
 )
 
 from app import views
@@ -16,16 +11,6 @@ from app import views
 urlpatterns = [
     # ================================================================================================================
     path("users/", UserAdminCreateView.as_view(), name="UserCreate"),
-    path("users/<int:user_id>/", UserAdminUpdateView.as_view(), name="UserUpdate"),
+    path("users/<str:user_id>/", UserAdminUpdateView.as_view(), name="UserUpdate"),
     # ================================================================================================================
-    path("stacks/", StackAdminCreateView.as_view(), name="StackCreate"),
-    path("stacks/<int:stack_id>/", StackAdminUpdateView.as_view(), name="StackUpdate"),
-    # ================================================================================================================
-    path("interns/", views.InternsView.as_view(), name="InternCreate"),
-    path("interns/<int:intern_id>/update/", InternAdminUpdateView.as_view(), name="InternUpdate"),
-    # ================================================================================================================
-   
-    # ================================================================================================================
-
-    
 ]
