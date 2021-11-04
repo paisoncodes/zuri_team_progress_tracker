584077de8e719597b0408440f1f11aefa7b226a5
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2021-11-04 15:22:25 -0700

    Improvement of responsiveness

--- frontend/src/components/ExperienceTracker.vue
+++ frontend/src/components/ExperienceTracker.vue
@@ -1,97 +1,107 @@
 <template>
-  <div class="text-left mx-auto md:w-full">
+  <div class="mx-auto md:w-full text-left ">
     <div class="flex justify-end">
       <p class="pb-5">
         Total Intern Count: {{getAllInterns.length}} / {{yearFinalists[0]}}
       </p>    
     </div>
-    <div class="sm:grid md:grid-cols-4 bg-brand-red-light-3 mb-5" v-for="(intern, index) in getAllInterns" :key="index" >
+    <div class="sm:grid md:grid-cols-4 mb-8 bg-brand-red-light-3" v-for="(intern, index) in getAllInterns" :key="index" >
       <div class="h-72">
-        <img class="icon w-full h-full object-cover" :src="intern.picture" alt="">
+        <img class="w-full h-full object-cover" :src="intern.picture" alt="">
       </div>
       
       <div class="md:col-span-3 mx-8 mt-5">
-        <div class="">
-          <div class="grid md:grid-cols-2 gap-4">
-            <div class="w-full text-brand-gray-dark-1 font-normal">
-              <p class="text-xl">{{intern.full_name}}</p>
-              <p class="text-base">{{intern.stack.join(', ')}}</p>
-            </div>
-            <div class="w-full">
-              <p class="leading-tight text-brand-gray-dark-2">{{intern.about}}</p>
-            </div>          
+        <div class="grid md:grid-cols-2 md:gap-4">
+          <div class="w-full text-brand-gray-dark-1 font-normal">
+            <p class="mb-2 md:mb-0 text-xl">{{intern.full_name}}</p>
+            <p class="mb-2 md:mb-0  text-base">{{intern.stack.join(', ')}}</p>
           </div>
-          <div>
-            <h3 class="my-4 text-brand-gray-dark-2">EXPERIENCE TRACKER</h3>
-            <!-- Large screen sizes -->
-            <div class="hidden lg:block px-4 py-3"> 
-              <div class="flex items-center justify-between space-x-3 text-center w-11/12 my-10 h-3 text-brand-gray-dark-2 bg-brand-gray-dark-3">
+          <div class="w-full">
+            <p class="leading-tight text-brand-gray-dark-2">{{intern.about}}</p>
+          </div>          
+        </div>
+        <div class="py-2">
+          <h3 class="my-4 text-brand-gray-dark-2">EXPERIENCE TRACKER</h3>
+          <!-- Large screen sizes -->
+          <div class="hidden lg:block px-4 py-3"> 
+            <div class="flex items-center justify-between space-x-3 text-center w-11/12 my-10 h-3 text-brand-gray-dark-2 bg-brand-gray-dark-3">
 
-                <div class="relative h-9 w-9 grid justify-items-center">
-                  <div class="h-9 w-9 rounded-full bg-brand-gray-dark-3"></div>
-                  <div class="absolute top-12">JOBLESS</div>            
-                </div>
-                <div class="flex flex-wrap relative bottom-6">2017</div>
-
-                <div class="relative grid justify-items-center h-12 w-12">
-                  <img src="@/assets/paypal.png" class="border-solid border-2 border-brand-gray-dark-3 h-12 w-12 rounded-full" alt="">
-                  <div class="absolute top-14">PAYPAL</div>
-                </div>
-                <div class="relative bottom-6">2018</div>
-
-                <div class="relative grid justify-items-center h-12 w-12">
-                  <img src="@/assets/flutterwave.png" class="h-12 w-12 border-solid border-2 border-brand-gray-dark-3 rounded-full" alt="">
-                  <div class="absolute top-14">FLUTTERWAVE</div>
-                </div>
-                <div class="relative bottom-6">2019</div>
-
-                <div class="relative grid justify-items-center h-12 w-12">
-                  <img src="@/assets/paystack.png" class="h-12 w-12 border-solid border-2 border-brand-gray-dark-3  rounded-full" alt="">
-                    <div class="absolute top-14">PAYSTACK</div>
-                </div>
-                <div  class="relative bottom-6">2020</div>
-
-                <div class="relative h-24 w-24 grid justify-items-center">
-                  <img src="@/assets/hotels.png" class="h-24 w-24 border-solid border-2 border-brand-gray-dark-1  rounded-full" alt="">
-                </div>
+              <div class="relative h-9 w-9 grid justify-items-center">
+                <div class="h-9 w-9 rounded-full bg-brand-gray-dark-3"></div>
+                <div class="absolute top-12">JOBLESS</div>            
               </div>
-            </div>
-            <!-- Smaller screen sizes -->
-            <div class="block lg:hidden px-4 py-3"> 
-              <div class="flex items-center justify-between space-x-3 text-center w-11/12 my-10 h-3 text-brand-gray-dark-2 bg-brand-gray-dark-3">
+              <div class="flex flex-wrap relative bottom-6">2017</div>
 
-                <div class="relative h-9 w-9 grid justify-items-center">
-                  <div class="h-9 w-9 rounded-full bg-brand-gray-dark-3"></div>
-                  <div class="absolute top-12">JOBLESS</div>            
-                </div>
-                <div class="relative bottom-6">2018</div>
+              <div class="relative grid justify-items-center h-12 w-12">
+                <img src="@/assets/paypal.png" class="border-solid border-2 border-brand-gray-dark-3 h-12 w-12 rounded-full" alt="">
+                <div class="absolute top-14">PAYPAL</div>
+              </div>
+              <div class="relative bottom-6">2018</div>
 
-                <div class="relative grid justify-items-center h-12 w-12">
-                  <img src="@/assets/paypal.png" class="border-solid border-2 border-brand-gray-dark-3 h-12 w-12 rounded-full" alt="">
-                  <div class="absolute top-14">PAYPAL</div>
-                </div>
-                <div class="relative bottom-6">2019</div>
+              <div class="relative grid justify-items-center h-12 w-12">
+                <img src="@/assets/flutterwave.png" class="h-12 w-12 border-solid border-2 border-brand-gray-dark-3 rounded-full" alt="">
+                <div class="absolute top-14">FLUTTERWAVE</div>
+              </div>
+              <div class="relative bottom-6">2019</div>
 
-                <div class="relative grid justify-items-center h-12 w-12">
-                  <img src="@/assets/flutterwave.png" class="h-12 w-12 border-solid border-2 border-brand-gray-dark-3 rounded-full" alt="">
-                  <div class="absolute top-14">FLUTTERWAVE</div>
-                </div>
-                <div class="relative bottom-6">2020</div>
+              <div class="relative grid justify-items-center h-12 w-12">
+                <img src="@/assets/paystack.png" class="h-12 w-12 border-solid border-2 border-brand-gray-dark-3  rounded-full" alt="">
+                  <div class="absolute top-14">PAYSTACK</div>
+              </div>
+              <div  class="relative bottom-6">2020</div>
 
-                <div class="relative h-24 w-24 grid justify-items-center">
-                  <img src="@/assets/hotels.png" class="h-24 w-24 border-solid border-2 border-brand-gray-dark-1  rounded-full" alt="">
-                </div>
+              <div class="relative h-24 w-24 grid justify-items-center">
+                <img src="@/assets/hotels.png" class="h-24 w-24 border-solid border-2 border-brand-gray-dark-1  rounded-full" alt="">
               </div>
             </div>
           </div>
-          <p @click="showLogin(intern.id)" class="float-right text-blue-500 cursor-pointer w-100 flex mb-3"  >
+          <!-- Smaller screen sizes e.g Tabs -->
+          <div class="hidden sm:block lg:hidden px-4 py-3"> 
+            <div class="flex items-center justify-between space-x-3 text-center w-11/12 my-10 h-3 text-brand-gray-dark-2 bg-brand-gray-dark-3">
+
+              <div class="relative h-9 w-9 grid justify-items-center">
+                <div class="h-9 w-9 rounded-full bg-brand-gray-dark-3"></div>
+                <div class="absolute top-12">JOBLESS</div>            
+              </div>
+              <div class="relative bottom-6">2018</div>
+
+              <div class="relative grid justify-items-center h-12 w-12">
+                <img src="@/assets/paypal.png" class="border-solid border-2 border-brand-gray-dark-3 h-12 w-12 rounded-full" alt="">
+                <div class="absolute top-14">PAYPAL</div>
+              </div>
+              <div class="relative bottom-6">2019</div>
+
+              <div class="relative grid justify-items-center h-12 w-12">
+                <img src="@/assets/flutterwave.png" class="h-12 w-12 border-solid border-2 border-brand-gray-dark-3 rounded-full" alt="">
+                <div class="absolute top-14">FLUTTERWAVE</div>
+              </div>
+              <div class="relative bottom-6">2020</div>
+
+              <div class="relative h-24 w-24 grid justify-items-center">
+                <img src="@/assets/hotels.png" class="h-24 w-24 border-solid border-2 border-brand-gray-dark-1  rounded-full" alt="">
+              </div>
+            </div>
+          </div>
+          <!-- Mobile screen size -->
+          <div class="flex space-x-3 justify-between sm:hidden">
+            <div><img class="w-10 rounded-full" src="@/assets/paystack.png" alt=""></div>
+            <div><img class="w-10 rounded-full" src="@/assets/flutterwave.png" alt=""></div>
+            <div><img class="w-10 rounded-full" src="@/assets/paypal.png" alt=""></div>
+            <div><img class="w-10 rounded-full" src="@/assets/hotels.png" alt=""></div>
+            <div><img class="w-10 rounded-full" src="@/assets/paystack.png" alt=""></div>
+          </div>
+          <!-- Edit button -->
+          <div @click="showLogin(intern.id)" class="flex justify-end text-blue-500 cursor-pointer w-100 mt-3"  >
             <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" clip-rule="evenodd" d="M14 1L19 6L6 19H1V14L14 1Z" stroke="#4774E8" stroke-width="1.22693" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg> <span class="ml-3">Edit</span>
-          </p>
+            </svg> 
+            <div class="ml-3">Edit</div>
+          </div>
         </div>
+
       </div>
     </div>
+    <!-- Pagination -->
     <div class="flex flex-col items-center">
       <div class="inline-flex mt-2 xs:mt-0">
         <button @click="handlePrev" :class="{'hidden': !prev}" class="cursor-pointer mx-3 bg-brand-gray-dark-1 hover:bg-brand-gray-dark-2 text-white text-sm font-medium rounded py-2 px-4">
@@ -213,21 +223,5 @@ export default {
   background-repeat: no-repeat;  
   background-size: cover; 
   background-position: center;
-}
-/* custom scrollbar */
-::-webkit-scrollbar {
-  width: 16px;
-  height: 5px;
-}
-/* Handle */
-::-webkit-scrollbar-thumb {
-  background: #898989; 
-  border-radius: 10px;
-  scrollbar-width: thin;
-}
-/* Handle on hover */
-::-webkit-scrollbar-thumb:hover {
-  background: #514949; 
-  transition:all 4s;
 }
 </style>
