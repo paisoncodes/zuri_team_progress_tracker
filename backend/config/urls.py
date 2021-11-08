29b3a42df41d39f2a343cb32c51853d5ca7119fe
paisoncodes <iamkomolafe.o.s@gmail.com>
2021-11-08 17:20:37 +0100

    small fix

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls import url
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from django.views.decorators.csrf import csrf_exempt
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -42,15 +43,15 @@ urlpatterns = [
 urlpatterns += [
     url(
         r"^swagger(?P<format>\.json|\.yaml)$",
-        schema_view.without_ui(cache_timeout=0),
+        csrf_exempt(schema_view.without_ui(cache_timeout=0)),
         name="schema-json",
     ),
     url(
         r"^api/v1/docs/$",
-        schema_view.with_ui("swagger", cache_timeout=0),
+        csrf_exempt(schema_view.with_ui("swagger", cache_timeout=0)),
         name="schema-swagger-ui",
     ),
     url(
-        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+        r"^redoc/$", csrf_exempt(schema_view.with_ui("redoc", cache_timeout=0)), name="schema-redoc"
     ),
 ]
