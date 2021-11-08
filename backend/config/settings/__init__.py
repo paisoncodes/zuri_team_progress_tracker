5073327b391278c35099b0f4137803704ba21d21
paisoncodes <iamkomolafe.o.s@gmail.com>
2021-11-08 13:29:14 +0100

    fixed env

--- backend/config/settings/__init__.py
+++ backend/config/settings/__init__.py
@@ -4,6 +4,6 @@ from decouple import config
 # you need to set "env = 'prod'" as an environment variable
 # in your OS (on which your website is hosted)
 if config("env") == "prod":
-    from .development import *
-else:
     from .production import *
+else:
+    from .development import *
